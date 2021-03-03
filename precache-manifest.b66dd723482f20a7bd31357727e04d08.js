4bdf554f405eee3febe0944046c7c82945526835
Tomabage <tamaoona@163.com>
2021-03-03 18:56:51 +0800

    deploy

---
+++ precache-manifest.b66dd723482f20a7bd31357727e04d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edd5c751afad65a7817",
+    "url": "/danlan-website/css/app.9cd29c2a.css"
+  },
+  {
+    "revision": "86f9b336495f165f7908dd77220d9e9f",
+    "url": "/danlan-website/index.html"
+  },
+  {
+    "revision": "8edd5c751afad65a7817",
+    "url": "/danlan-website/js/app.4aa97dd3.js"
+  },
+  {
+    "revision": "f18cfcc12e5198afd63a",
+    "url": "/danlan-website/js/chunk-vendors.93805bc4.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/danlan-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/danlan-website/robots.txt"
+  }
+]);
